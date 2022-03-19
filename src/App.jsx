f1cf3eb8321b142d631cd41da590c52cba997b47
jumpei <52040507+jumpei-jp@users.noreply.github.com>
2022-03-19 10:32:00 +0900

    Update: alertやstyleの追加 表示画面に文字の装飾やalertを出すようにしてみた。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 const App = () => {
+  //ボタンをクリックした時の動作 アラートを表示
+  const onClickButtton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   //複数タグをレンダリングする時は()を使って複数のタグを囲む
   //()の中身は一つのタグで囲まないといけない
   return (
     <React.Fragment>
-      <h1>hello</h1>
-      <p>how are you?</p>
+      <h1 style={{ color: "red" }}>hello</h1>
+      <p style={contentStyle}>how are you?</p>
+      <button onClick={onClickButtton}>ボタン</button>
     </React.Fragment>
   );
 };
